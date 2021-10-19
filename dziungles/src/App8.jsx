4c60a0e964545f54f7d0317cd4ff1f4a4878d9b9
gretapos <86227872+gretapos@users.noreply.github.com>
2021-10-19 12:14:27 +0300

    nd3

---
+++ dziungles/src/App8.jsx
@@ -0,0 +1,15 @@
+import Apskritimai from "./Components/Apskritimai";
+
+
+
+function App() {
+
+    return(
+        <>
+            <h3>Namu darbas 3</h3>
+            <Apskritimai/>
+        </>
+    )
+}
+
+export default App;
