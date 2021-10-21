4469ca2c92c255d32d2b1cf14816cac1d8e8e25f
gretapos <86227872+gretapos@users.noreply.github.com>
2021-10-21 12:33:45 +0300

    delete and ids

--- dziungles/src/Components/FieldAnimal.jsx
+++ dziungles/src/Components/FieldAnimal.jsx
@@ -1,21 +1,24 @@
-function FieldAnimal({ fieldAnimal, field }) {
+function FieldAnimal({ fieldAnimal, field, goHome }) {
 
 
     if (field === fieldAnimal.field && 'cow' === fieldAnimal.animal) {
         return (
-            <div className="cow">
+            <div className="cow" onClick={() => goHome(fieldAnimal.id)}>
+                <h2>{fieldAnimal.id}</h2>
             </div>
         );
     }
     else if (field === fieldAnimal.field && 'sheep' === fieldAnimal.animal) {
         return (
-            <div className="sheep">
+            <div className="sheep" onClick={() => goHome(fieldAnimal.id)}>
+                <h2>{fieldAnimal.id}</h2>
             </div>
         );
     }
     else if (field === fieldAnimal.field && 'horse' === fieldAnimal.animal) {
         return (
-            <div className="horse">
+            <div className="horse" onClick={() => goHome(fieldAnimal.id)}>
+                <h2>{fieldAnimal.id}</h2>
             </div>
         );
     }
--- dziungles/src/app.jsx
+++ dziungles/src/app.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import FieldAnimal from "./Components/FieldAnimal";
+import idGenerator from "./Common/idGenerator";
 
 
 function App() {
@@ -9,8 +10,13 @@ function App() {
 
     const add = (what) => {
         const fieldCopy = field.slice();
-        fieldCopy.push({ animal: what, field: parseInt(fieldNamber) });
+        fieldCopy.push({
+            id: idGenerator(),
+            animal: what,
+            field: parseInt(fieldNamber)
+        });
         setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy))
         console.log(fieldCopy);
     }
 
@@ -18,32 +24,48 @@ function App() {
         setFieldNamber(e.target.value);
     }
 
-    // useEffect(() => {
-    //     console.log('susirenderino')
-    // }, []);
+    const goHome = (id) => {
+        const fieldCopy = field.slice();
+        fieldCopy.forEach((a, i) => {
+            if (a.id === id) {
+                fieldCopy.splice(i, 1); 
+            }
+        });
+        setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy))
+    }
+
+    useEffect(() => {
+        const animalsFromStorage = localStorage.getItem('animals');
+        if (null !== animalsFromStorage) {
+            setField(JSON.parse(animalsFromStorage));
+        }
+    }, []);
+
+
 
     return (
         <>
             <div className="field">
                 <div className="field__part">
-                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={1} fieldAnimal={fieldAnimal}></FieldAnimal>)}
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={1} fieldAnimal={fieldAnimal} goHome={goHome}></FieldAnimal>)}
                 </div>
                 <div className="field__part">
-                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={2} fieldAnimal={fieldAnimal}></FieldAnimal>)}
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={2} fieldAnimal={fieldAnimal} goHome={goHome}></FieldAnimal>)}
                 </div>
                 <div className="field__part">
-                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={3} fieldAnimal={fieldAnimal}></FieldAnimal>)}
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={3} fieldAnimal={fieldAnimal} goHome={goHome}></FieldAnimal>)}
                 </div>
             </div>
             <div className="buttons-holder">
-            <button onClick={() => add('cow')}>Add cow</button>
-            <button onClick={() => add('sheep')}>Add sheep</button>
-            <button onClick={() => add('horse')}>Add horse</button>
-            <select value={fieldNamber} onChange={selectField}>
-                <option value={1}>Field One</option>
-                <option value={2}>Field Two</option>
-                <option value={3}>Field Three</option>
-            </select>
+                <button onClick={() => add('cow')}>Add cow</button>
+                <button onClick={() => add('sheep')}>Add sheep</button>
+                <button onClick={() => add('horse')}>Add horse</button>
+                <select value={fieldNamber} onChange={selectField}>
+                    <option value={1}>Field One</option>
+                    <option value={2}>Field Two</option>
+                    <option value={3}>Field Three</option>
+                </select>
             </div>
         </>
     );
