3a712069a08ae0fe8f448f88753f76f67605d64a
gretapos <86227872+gretapos@users.noreply.github.com>
2021-10-26 12:21:54 +0300

    serv

--- dziungles/server/app.js
+++ dziungles/server/app.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3003
+const mysql = require('mysql');
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json());
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "zmones",
+  password: "zmones1234"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Labas, ka tu?')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/labas/', (req, res) =>{
+  
 })
+
+//app.listen(port, () => {
+//  console.log(`Example app listening at http://localhost:${port}`)
+//})
